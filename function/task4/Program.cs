1a31f6eeeba1c9eeb9d63b43b96b2cff58b66cd9
Darya <zhizhinada@yandex.ru>
2024-01-18 20:57:57 +0400

    task4

---
+++ function/task4/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Определите количество простых числел в этом массиве
+
+// Создание массива
+int[] CreateArray(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for(int i = 0; i < size; i++)
+    {
+        
+        arr[i] = rnd.Next(0, 100);
+    }
+    return arr;
+}
+
+
+//Поиск простых чисел
+bool IsSimple(int n)
+{
+    for( int i = 2; i < n; i++)
+    {
+        if(n % i == 0)
+        {
+            return false;
+        }
+        
+    }
+    return true;
+}
+
+// Подсчет простых чисел
+int GetPrimeQuantity(int[] array)
+{
+    int countPrimes = 0;
+    foreach(int e in array)
+    {
+        bool result = IsSimple(e);
+        if(result == true)
+        {
+            countPrimes = countPrimes + 1;
+        }
+        
+    }
+    return countPrimes;
+}
+
+Console.WriteLine("size");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArray(n);
+
+int count = GetPrimeQuantity(arr);
+
+Console.WriteLine(count);
