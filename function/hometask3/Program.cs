a8af764c70344ddd01ca85d7af5228d371dd3b22
Darya <zhizhinada@yandex.ru>
2024-01-19 18:52:49 +0400

    add hometask3

---
+++ function/hometask3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+int[] numbers = { 10, 9, 7, 45, 25, 3 };
+int temp;
+
+Console.Write("Исходный массив: ");
+foreach(int number in numbers)
+{
+    Console.Write(number + " ");
+}
+
+for(int i = 0; i < 6; i++)
+{
+    temp = numbers[i];
+    numbers[i] = numbers[6 - 1 - i];
+    numbers[6 - 1 - i] = temp;
+}
+Console.Write("Перевернутый массив: ");
+foreach(int number in numbers)
+{
+    Console.Write(number + " ");
+}
