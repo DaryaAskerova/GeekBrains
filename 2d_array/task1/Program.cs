68d883ab1bf34aab5ccf425b3c03a2986528fad1
Darya <zhizhinada@yandex.ru>
2024-01-25 12:58:25 +0400

    add task1

---
+++ 2d_array/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Создать двумерный массив с размером 3х5, состоящий из целых чисел. Вывести элементы на экран
+
+int[,] matrix = new int[3, 5];
+Random rnd = new Random();
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        matrix[i, j] = rnd.Next(1, 11);
+    }
+}
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine(); // Переход на новую строку, чтобы массив вышел в виде таблицы
+}
