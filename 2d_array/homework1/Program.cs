a4b86b793750c58c945ebee9ab38953a519adc7e
Darya <zhizhinada@yandex.ru>
2024-01-26 15:42:32 +0400

    add homework1

---
+++ 2d_array/homework1/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+int[,] numbers = new int[,] {
+    {1, 2, 3},
+    {1, 1, 0},
+    {7, 8, 2},
+    {9, 10, 11}
+};
+
+/// Вычисление сумм по строкам (на выходе массив с суммами строк)
+int[] SumRows(int[,] array)
+{
+    int rowCount = array.GetLength(0);
+      
+    int[] sums = new int[rowCount];
+      
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        int rowSum = 0;
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+          rowSum = rowSum + array[i,j];
+          
+        }
+         sums[i] = rowSum;
+    }
+    return sums;
+     
+}
+    
+// Получение индекса минимального элемента в одномерном массиве
+int MinIndex(int[] array)
+{
+    int imin = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < array[imin])
+        {
+          
+          imin = i;
+        }
+    }
+    return imin;
+}
+   
+  
+int[] sums = SumRows(numbers);
+int index = MinIndex(sums);
+Console.WriteLine(index);
+
