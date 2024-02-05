be493314a98ad819bea7b9ab13b3f1c3d0a93f50
Darya <zhizhinada@yandex.ru>
2024-02-05 10:01:38 +0400

    add task5

---
+++ string/task5/Program.cs
@@ -0,0 +1,17 @@
+﻿// На основе символов строки (типа string) сформировать массив симыолов (тиа char[]). Вывести массив на экран
+char[] StringToCharArray(string str)
+{
+    char[] chars = new char[str.Length];
+    for(int i = 0; i < str.Length; i++)
+    {
+        chars[i] = str[i];
+    }
+    return chars;
+}
+
+string str = "Hello world!";
+char[] chars = StringToCharArray(str);
+foreach(char e in chars)
+{
+    Console.WriteLine(e);
+}
