20080927d86108902461906786f0ceb5c8532564
Darya <zhizhinada@yandex.ru>
2024-02-05 09:37:51 +0400

    add task4

---
+++ string/task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Создайте массив символов (тип char[]). Создайте строку из символов этого массива.
+string GetStringFromCharArray(char[] array)
+{
+    string str = "";
+    foreach(char e in array)
+    {
+        str += e;
+    }
+    return str;
+}
+
+char[] chars = {'a', 'b', 'c', 'd'};
+string str = GetStringFromCharArray(chars);
+Console.WriteLine(str);
+
