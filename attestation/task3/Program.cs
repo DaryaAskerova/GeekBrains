b727f471c07ecf9a0896fa8bfae7b646ade8b222
Darya <zhizhinada@yandex.ru>
2024-02-05 14:01:52 +0400

    add task3

---
+++ attestation/task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+void ReversArray (int[] array, int arr)
+{
+    if(arr > array.Length)
+    {
+        Console.WriteLine("Stop");
+        return;
+    }
+    ReversArray (array, arr + 1);
+    Console.Write($"{arr} ");
+
+}
+int[] array = {1, 2, 3, 4, 5, 6, 7};
+ReversArray (array, 1);
