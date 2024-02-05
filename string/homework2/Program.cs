3adfea80a5af6738f72088161b435b2b36f037d3
Darya <zhizhinada@yandex.ru>
2024-02-05 11:03:16 +0400

    add homework2

---
+++ string/homework2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+
+string GetLettersLowercase(char[] array)
+{
+    string str = "";
+    foreach(char e in array)
+    {
+        str += e;
+    }
+    str = str.ToLower();
+    return str;
+}
+
+char[] chars = {'H', 'E', 'L', 'L', 'O'};
+string str = GetLettersLowercase(chars);
+Console.WriteLine(str);
