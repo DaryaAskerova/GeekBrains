fe16be2c1d0e6354ce2ceceb35a5fec9c7db406b
Darya <zhizhinada@yandex.ru>
2024-02-05 10:44:39 +0400

    add homework1

---
+++ string/homework1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+string GetStringFromCharArray(char[] array)
+{
+    string str = "";
+    foreach(char e in array)
+    {
+        str += e;
+    }
+    return str;
+}
+
+char[] chars = {'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'};
+string str = GetStringFromCharArray(chars);
+Console.WriteLine(str);
